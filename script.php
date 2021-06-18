842ea1a5510fbf07d92eec2cb241fc16fc9f0fb6
Fadi Albahrani <business.fadi21@gmail.com>
2021-06-18 09:13:35 +0300

    initializing commit

---
+++ script.php
@@ -0,0 +1,68 @@
+<?php
+
+
+
+$servername = "localhost";
+$username = "root";
+$password = "";
+$databaseName = "roboticarm";
+
+
+// create connection to mySql
+$conn = new mysqli($servername, $username, $password, $databaseName);
+
+//check connection
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+  } else {
+
+   save($conn);
+
+  }
+  
+
+  function updateDB ($conn) {
+    $sql = "UPDATE motors SET degree='170' WHERE id=1";
+
+    if ($conn->query($sql) === TRUE) {
+      echo "Record updated successfully";
+    } else {
+      echo "Error updating record: " . $conn->error;
+    }
+  }
+
+  function save ($conn){
+
+    $motor1 =  $_GET['motor1'];
+    $motor2 =  $_GET['motor2'];
+    $motor3 =  $_GET['motor3'];
+    $motor4 =  $_GET['motor4'];
+    $motor5 =  $_GET['motor5'];
+    $motor6 =  $_GET['motor6'];
+
+
+    // $sql = "UPDATE motors SET degree= $motor1  WHERE id=1";
+    // $sq2 = "UPDATE motors SET degree= $motor2  WHERE id=2";
+    // $sq3 = "UPDATE motors SET degree= $motor3  WHERE id=3";
+    // $sql = "UPDATE motors SET degree= $motor4  WHERE id=4";
+    // $sql = "UPDATE motors SET degree= $motor5  WHERE id=5";
+    // $sql = "UPDATE motors SET degree= $motor6  WHERE id=6";
+
+    mysqli_query($conn, "UPDATE motors SET degree= $motor1  WHERE id=1", 0);
+    mysqli_query($conn, "UPDATE motors SET degree= $motor2  WHERE id=2", 0);
+    mysqli_query($conn, "UPDATE motors SET degree= $motor3  WHERE id=3", 0);
+    mysqli_query($conn, "UPDATE motors SET degree= $motor4  WHERE id=4", 0);
+    mysqli_query($conn, "UPDATE motors SET degree= $motor5  WHERE id=5", 0);
+    mysqli_query($conn, "UPDATE motors SET degree= $motor6  WHERE id=6", 0);
+
+    
+    // if ($conn->query($sql) === TRUE) {
+    //   echo "Record updated successfully";
+    // } else {
+    //   echo "Error updating record: " . $conn->error;
+    // }
+    
+  }
+
+?>
